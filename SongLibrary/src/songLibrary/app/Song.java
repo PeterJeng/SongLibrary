dac80f28fdb71544b391109a67a4df11b04b12bf
Peter <Peter@PETER-LAPTOP>
2017-09-25 21:33:11 -0400

    created toString() method that returns name, artist, album, and year

--- SongLibrary/src/songLibrary/app/Song.java
+++ SongLibrary/src/songLibrary/app/Song.java
@@ -3,4 +3,10 @@ package songLibrary.app;
 public class Song {
 	public String name;
 	public String artist;
+	public String album;
+	public String year;
+	
+	public String toString() {
+		return name + "," + artist + "," + album + "," + year;
+	}
 }
