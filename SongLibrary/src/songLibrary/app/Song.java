ee2b8d6204fc0e88630a031fcd7b334a968bb7b5
Peter <Peter@PETER-LAPTOP>
2017-09-26 01:16:50 -0400

    Created a setFields(String s)
    This method takes a pre-formatted string and form
    "name,artist,album,year" and initializes the global variables by
    concatenating the string

--- SongLibrary/src/songLibrary/app/Song.java
+++ SongLibrary/src/songLibrary/app/Song.java
@@ -18,4 +18,15 @@ public class Song {
 	public String toString() {
 		return name + "," + artist + "," + album + "," + year;
 	}
+	
+	public void SetFields(String information) {
+		int firstComma = information.indexOf(",", 0);
+		int secondComma = information.indexOf(",", firstComma + 1);
+		int thirdComma = information.indexOf(",", secondComma + 1);
+		
+		this.name = information.substring(0, firstComma);
+		this.artist = information.substring(firstComma + 1, secondComma);
+		this.album = information.substring(secondComma + 1, thirdComma);
+		this.year = information.substring(thirdComma + 1);
+	}
 }
