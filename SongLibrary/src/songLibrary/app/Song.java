a03b9b8ee175ba424c18f46e1035e48ed8eb469e
Peter <Peter@PETER-LAPTOP>
2017-10-04 01:31:59 -0400

    Update to the song model structure of the program
    1)Implemented a interface Comparable that compares the name and artist
    of the song
    
    2)Changed delimiter from ',' to ';'

--- SongLibrary/src/songLibrary/app/Song.java
+++ SongLibrary/src/songLibrary/app/Song.java
@@ -1,6 +1,6 @@
 package songLibrary.app;
 
-public class Song {
+public class Song implements Comparable<Song>{
 	public String name;
 	public String artist;
 	public String album;
@@ -14,19 +14,41 @@ public class Song {
 		this.album = album;
 		this.year = year;
 	}
-	
-	public String toString() {
-		return name + "," + artist + "," + album + "," + year;
-	}
-	
-	public void setFields(String information) {
-		int firstComma = information.indexOf(",", 0);
-		int secondComma = information.indexOf(",", firstComma + 1);
-		int thirdComma = information.indexOf(",", secondComma + 1);
+
+	//compareTo method that will be used in Collection.sort
+	//sorts in ascending order
+	@Override
+	public int compareTo(Song songToCompare) {
+		//checks to see if the names are equal, if they are compare by artist, else compare by name
 		
-		this.name = information.substring(0, firstComma);
-		this.artist = information.substring(firstComma + 1, secondComma);
-		this.album = information.substring(secondComma + 1, thirdComma);
-		this.year = information.substring(thirdComma + 1);
+		if(this.name.equals(songToCompare.name))
+			return this.artist.compareTo(songToCompare.artist);
+		else
+			return this.name.compareTo(songToCompare.name);
 	}
+	
+	/*
+	 * Given a string that contains name;artist;album;year
+	 * Sets the instance field of the current song object to the respective fields in the string
+	 */
+	public void setFields(String information) {
+		int firstColon = information.indexOf(";", 0);
+		int secondColon = information.indexOf(";", firstColon + 1);
+		int thirdColon = information.indexOf(";", secondColon + 1);
+		
+		this.name = information.substring(0, firstColon);
+		this.artist = information.substring(firstColon + 1, secondColon);
+		this.album = information.substring(secondColon + 1, thirdColon);
+		this.year = information.substring(thirdColon + 1);
+	}	
+	
+	/*
+	 * Overrides the toString() method to return the Song's fields
+	 * Separates the song by delimiters of ;
+	 */
+	public String toString() {
+		return name + ";" + artist + ";" + album + ";" + year;
+	}
+
+
 }
